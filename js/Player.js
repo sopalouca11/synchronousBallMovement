2137a04c3ce2d40067d398d6d549b2f29457b9f7
sopalouca11 <amj582008@gmail.com>
2022-05-11 14:14:04 -0300

    first comit

---
+++ js/Player.js
@@ -0,0 +1,31 @@
+class player{
+
+    constructor(){}
+getCount(){
+
+    var playerCountRef = database.ref('playerCount')
+    playerCountRef.on("value",function(data){
+
+        playerCount = data.val();
+
+    })
+
+}
+    updateCount(count){
+
+        database.ref('/').update({
+
+            playerCount:count
+        })
+    }
+    update(name){
+
+        var playerIndex = "player"+playerCount;
+        database.ref(playerIndex).set({
+
+            name:name
+
+        })
+
+    }
+}
